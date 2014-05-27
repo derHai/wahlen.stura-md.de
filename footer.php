41a0f57eace79b320fd7b9bee5459c5a2dfd82dd
Johannes Filter <filter@st.ovgu.de>
2014-05-28 01:45:13 +0200

    switching to HTML, no PHP needed

--- footer.php
+++
@@ -1,9 +0,0 @@
-		<div class="clear"></div>
-		<footer>
-			<div>
-					<a href="http://www.avmz.ovgu.de"> Foto: OVGU/AVMZ</a> -	<a href="http://www.stura-md.de/Impressum">Impressum</a>
-			</div>
-		</footer>
-
-	</body>
-</html>
